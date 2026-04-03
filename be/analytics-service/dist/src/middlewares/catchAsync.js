6178ab31ca191ebd10765e3d6c492954f2695754
tranvinhhuy04 <tranvinhhuy04@gmail.com>
2026-04-03 10:53:32 +0700

    fix docker

--- be/analytics-service/dist/src/middlewares/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsync = void 0;
-const catchAsync = (handler) => {
-    return (req, res, next) => {
-        Promise.resolve(handler(req, res, next)).catch(next);
-    };
-};
-exports.catchAsync = catchAsync;
